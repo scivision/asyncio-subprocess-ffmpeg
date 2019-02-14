9095054080df213ba765fd56fdc7f907ff65bcb2
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-02-14 11:31:22 -0500

    init
    metadata
    
    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# asynchio-subprocess-ffmpeg
-Examples of Python asyncio.subprocess
+[![Build Status](https://travis-ci.com/ec500-software-engineering/asynchio-subprocess-ffmpeg.svg?branch=master)](https://travis-ci.com/ec500-software-engineering/asynchio-subprocess-ffmpeg)
+
+# asyncio FFmpeg
+
+Examples of Python asyncio.subprocess with FFmpeg
---
+++ ffmpeg.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import asyncio
+from pathlib import Path
+from typing import Generator
+import shutil
+from argparse import ArgumentParser
+
+FFPLAY = shutil.which('ffplay')
+if not FFPLAY:
+    raise FileNotFoundError('FFPLAY not found')
+
+
+async def ffplay(filein: Path):
+    """ Play media asynchronously """
+    assert isinstance(FFPLAY, str)
+
+    proc = await asyncio.create_subprocess_exec(FFPLAY, str(filein))
+
+    await proc.wait()
+
+
+async def main(flist: Generator[Path, None, None]):
+    await asyncio.gather(*[ffplay(f) for f in flist])
+
+
+if __name__ == '__main__':
+    p = ArgumentParser(
+        description="Plays media files asynchronously with FFplay")
+    p.add_argument('path', help='directory where media files are kept')
+    p.add_argument('-suffix', help='file suffixes of desired media file types',
+                   nargs='+', default=['.mp4', '.avi', '.ogv'])
+    P = p.parse_args()
+
+    path = Path(P.path).expanduser()
+    if not path.is_dir():
+        raise FileNotFoundError(f'{path} is not a directory')
+
+    flist = (f for f in path.iterdir() if f.is_file() and f.suffix in P.suffix)
+
+    asyncio.run(main(flist))
