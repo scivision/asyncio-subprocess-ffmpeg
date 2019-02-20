1a5d8a6ab6222b5b418d539266010c6b8f79fda0
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-02-19 22:54:05 -0500

    more file types

--- ffplay.py
+++ ffplay.py
@@ -21,7 +21,10 @@ if not FFPLAY:
 
 
 async def ffplay(queue: asyncio.Queue):
-    """ Play media asynchronously """
+    """
+    Play media asynchronously.
+    Each task runs endlessly until .cancel()
+    """
     assert isinstance(FFPLAY, str)
 
     while True:
@@ -67,7 +70,7 @@ if __name__ == '__main__':
         description="Plays media files asynchronously with FFplay")
     p.add_argument('path', help='directory where media files are kept')
     p.add_argument('-suffix', help='file suffixes of desired media file types',
-                   nargs='+', default=['.mp4', '.avi', '.ogv'])
+                   nargs='+', default=['.mp4', '.avi', '.ogv', '.wmv', '.flv', '.mov'])
     P = p.parse_args()
 
     path = Path(P.path).expanduser()
