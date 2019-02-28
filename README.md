40da12b6e52261bf78798c1f6edfb11a9a56fba3
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-02-27 21:58:33 -0500

    rename, doc

--- README.md
+++ README.md
@@ -7,5 +7,12 @@ In general to show the latest techniques, Python &ge; 3.7 is required.
 
 ## FFprobe
 
-* `probe_sync.py`: Use FFprobe to retrieve file metadata synchronously
-* `probe_async.py`: Use FFprobe to retrieve file metadata asynchronously using Python `asyncio`.
+* `probe_sync`: Use FFprobe to retrieve file metadata synchronously
+* `probe_coroutine`: Use FFprobe to retrieve file metadata asynchronously using Python `asyncio` coroutine event loop.
+
+## FFplay
+
+I like to test asynchronous techniques with video playback, as it makes some effects obvious.
+
+* `play_threadpool`: Even though coroutines are more efficient, the syntax of `concurrent.futures.ThreadPoolExecutor` is perhaps the simplest possible way to spawn independent processes in a controlled fashion
+* `play_coroutine`: Use Python `asyncio` coroutine event loop to spawn processes.
---
+++ play_threadpool.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from pathlib import Path
+import subprocess
+from argparse import ArgumentParser
+import concurrent.futures
+
+
+def ffplay(filein: Path):
+    assert filein.is_file()
+
+    cmd = ['ffplay', '-v', 'warning', '-autoexit', str(filein)]
+
+    subprocess.check_call(cmd)
+
+
+if __name__ == '__main__':
+    p = ArgumentParser(
+        description="Asynchronous playback with ThreadPool and FFplay")
+    p.add_argument('path', help='directory where media files are kept')
+    p.add_argument('-suffix', help='file suffixes of desired media file types',
+                   nargs='+', default=['.mp4', '.avi', '.ogv', '.wmv', '.flv', '.mov'])
+    P = p.parse_args()
+
+    path = Path(P.path).expanduser()
+    if not path.is_dir():
+        raise FileNotFoundError(f'{path} is not a directory')
+
+    flist = (f for f in path.iterdir() if f.is_file() and f.suffix in P.suffix)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2,
+                                               thread_name_prefix='ffplay') as pool:
+        pool.map(ffplay, flist)
